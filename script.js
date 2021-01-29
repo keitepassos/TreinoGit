49219f13d08c61be23656f1343cacc3cdbf3ced2
keite passos <keitepassos@gmail.com>
2021-01-29 16:20:56 -0300

    commit keite

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function clicar(){
     alert('Clicou no botão...ALTERADO!!!!!!!!!!!!')
 }
+
+
+var num1=document.getElementById('num1')
+var num2=document.getElementById('num2');
+var resultado = document.getElementById('resultado');
+
+function calcular(opcao=''){
+    
+    num1 = num1.value=='' ? 0:parseFloat(num1.value);
+    num2 = num2.value == '' ? 0:parseFloat(num2.value);
+
+    //resultado.innerHTML =  num1 + num2;
+
+    alert(num1 + num2)
+
+}
